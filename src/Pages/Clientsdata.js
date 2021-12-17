8a9e2b5449d77a7833043eb498169bb36bcf3ea4
Naveen <naveen@internetbusinesssolutionsindia.com>
2021-12-17 18:24:33 +0530

    first commit

--- src/Pages/Clientsdata.js
+++ src/Pages/Clientsdata.js
@@ -1,22 +1,13 @@
 export default [
     { 
-        "id": 1,
-        "sitename": "web do it",
-        "clientlogo": "./img/client1.png",
-        "description": 'Webuyit.dk makes a difference.Denmark throws every year 125,000 tons of electronics out – equivalent to 22.5 kilograms per.Dane. Webuyit.dk has partnered with DBA.dk.',
-        "beforesiteimg": "./img/client1.png",
-        "aftersiteimg": "./img/client1.png",
-        "technologies": "['PHP', 'HTML/CSS', 'jQuery/Ajax', 'Response', 'MySql']",
-        "sitelink":"www.google.com"
-     },
-     { 
-        "id":2,
-        "sitename": "future profilez",
-        "clientlogo": "./img/client1.png",
-        "description": 'Webuyit.dk makes a difference.Denmark throws every year 125,000 tons of electronics out – equivalent to 22.5 kilograms per.Dane. Webuyit.dk has partnered with DBA.dk.',
-        "beforesiteimg": "./img/client1.png",
-        "aftersiteimg": "./img/client1.png",
-        "technologies": "['PHP', 'HTML/CSS', 'jQuery/Ajax', 'Response', 'MySql']",
-        "sitelink":"www.google.com"
+        id: 1,
+        sitename: "web do it",
+        clientlogo: "./img/client1.png",
+        description: 'Webuyit.dk makes a difference.Denmark throws every year 125,000 tons of electronics out – equivalent to 22.5 kilograms per.Dane. Webuyit.dk has partnered with DBA.dk.',
+        beforesiteimg: "./img/client1.png",
+        aftersiteimg: "./img/client1.png",
+        technologies: "['PHP', 'HTML/CSS', 'jQuery/Ajax', 'Response', 'MySql']",
+        sitelink:"www.google.com"
      }
+    
  ];
